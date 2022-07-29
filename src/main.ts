225dacae63493bd88ea9b5a3dca636f6f6063201
Naveen Thurimerla <nawinto99@gmail.com>
2022-07-29 21:25:02 +0530

    :fire: add linting setup

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+function sayMyName(name: string): void {
+  if (name === 'Naveen') {
+    console.log("You're right 👍");
+  } else {
+    console.log("You're wrong 👎");
+  }
+}
+
+sayMyName('Naveen');
